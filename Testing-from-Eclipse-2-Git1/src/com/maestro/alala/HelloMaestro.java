191ddbe9e6b82aac8df87b3b40ab0685e4ea88a9
chariotoffire77 <kekeina920@gmail.com>
2019-06-30 22:37:08 -0400

    Added new output message

--- Testing-from-Eclipse-2-Git1/src/com/maestro/alala/HelloMaestro.java
+++ Testing-from-Eclipse-2-Git1/src/com/maestro/alala/HelloMaestro.java
@@ -6,6 +6,8 @@ public class HelloMaestro {
 		// TODO Auto-generated method stub
 
 		System.out.println("Hello this is a test, greeting from HelloMaestro 1......");
+		
+		System.out.println("Hi this another test......, greeting from HelloMaestro 1...");
 	}
 
 }
